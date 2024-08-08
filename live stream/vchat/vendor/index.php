7cba4c9c80134018dc544c1a97ccfcb46589ce3c
annm12345 <120537727+annm12345@users.noreply.github.com>
2024-08-09 00:39:24 +0630

    Add files via upload

---
+++ live stream/vchat/vendor/index.php
@@ -0,0 +1,13 @@
+<?php
+echo"Hello World";
+?>
+<script>
+    var conn = new WebSocket('ws://localhost:8080');
+    conn.onopen = function(e) {
+        console.log("Connection established!");
+    };
+
+    conn.onmessage = function(e) {
+        console.log(e.data);
+    };
+</script>
